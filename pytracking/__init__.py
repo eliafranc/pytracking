52225175ef1fd16754c639c4b0890a32d0c484a6
Elia Franc <efranc98@gmail.com>
2023-12-19 11:26:29 +0100

    Removed namespace changes. New branch ml-backend hold all of them now.

--- pytracking/__init__.py
+++ pytracking/__init__.py
@@ -1,8 +1,8 @@
-from pytracking.pytracking.libs import TensorList, TensorDict
-import pytracking.pytracking.libs.complex as complex
-import pytracking.pytracking.libs.operation as operation
-import pytracking.pytracking.libs.fourier as fourier
-import pytracking.pytracking.libs.dcf as dcf
-import pytracking.pytracking.libs.optimization as optimization
-from pytracking.pytracking.run_tracker import run_tracker
-from pytracking.pytracking.run_webcam import run_webcam
+from pytracking.libs import TensorList, TensorDict
+import pytracking.libs.complex as complex
+import pytracking.libs.operation as operation
+import pytracking.libs.fourier as fourier
+import pytracking.libs.dcf as dcf
+import pytracking.libs.optimization as optimization
+from pytracking.run_tracker import run_tracker
+from pytracking.run_webcam import run_webcam
