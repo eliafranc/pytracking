1114cbd2fbfe94f8877686b6f73e684b6efd2a68
Elia Franc <efranc98@gmail.com>
2024-03-12 22:42:27 +0100

    Code to overlay tensors in different ways.

--- pytracking/run_tensor.py
+++ pytracking/run_tensor.py
@@ -11,10 +11,10 @@ from pytracking.evaluation import Tracker
 """
 Test Files:
 
-'/home/efranc/data/2024_01_10_112814_drone_000/frames_ts.csv'
-'/home/efranc/data/2024_01_10_112814_drone_000/frames'
-'/home/efranc/data/2024_01_10_112814_drone_000/events_left_final.h5'
-'/home/efranc/data/2024_01_10_112814_drone_000/Projection_rgb_to_event_left.npy'
+'/home/efranc/data/2024_01_10_112814_drone_000/frames_ts.csv',
+'/home/efranc/data/2024_01_10_112814_drone_000/frames',
+'/home/efranc/data/2024_01_10_112814_drone_000/events_left_final.h5',
+'/home/efranc/data/2024_01_10_112814_drone_000/Projection_rgb_to_event_left.npy',
 '/home/efranc/data/2024_01_10_112814_drone_000/labels_events_left.npy'
 
 """
@@ -27,9 +27,10 @@ def run_tensor(tracker_name, tracker_param, timings_file, rgb_frame_dir, event_f
         ...
     """
     tracker = Tracker(tracker_name, tracker_param)
-    tracker.run_on_tensor(timings_file, rgb_frame_dir, event_file, homography_file, label_file , debug=debug, save_results=save_results)
+    tracker.run_on_tensor(timings_file, rgb_frame_dir, event_file, homography_file, label_file , debug=debug)
 
 def main():
+    """
     parser = argparse.ArgumentParser(description='Run the tracker on your webcam.')
     parser.add_argument('tracker_name', type=str, help='Name of tracking method.')
     parser.add_argument('tracker_param', type=str, help='Name of parameter file.')
@@ -45,6 +46,12 @@ def main():
     args = parser.parse_args()
 
     run_tensor(args.tracker_name, args.tracker_param, args.timings_file, args.rgb_frame_dir, args.event_file, args.homography_file, args.label_file, args.debug, args.save_results)
+    """
+    run_tensor('rts', 'rts50', '/home/efranc/data/2024_01_10_112814_drone_000/frames_ts.csv',
+        '/home/efranc/data/2024_01_10_112814_drone_000/frames',
+        '/home/efranc/data/2024_01_10_112814_drone_000/events_left_final.h5',
+        '/home/efranc/data/2024_01_10_112814_drone_000/Projection_rgb_to_events_left.npy',
+        '/home/efranc/data/2024_01_10_112814_drone_000/labels_events_left.npy', None, True)
 
 
 if __name__ == '__main__':
